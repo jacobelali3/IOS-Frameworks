c77cbeb505b738fcb9c63b981dc90a4552bd1dad
MHornerUts <79507075+MHornerUts@users.noreply.github.com>
2022-05-17 03:24:12 +1000

    Update FlightsManager.swift

--- Booking_App/Booking_App/Util/FlightsManager.swift
+++ Booking_App/Booking_App/Util/FlightsManager.swift
@@ -9,10 +9,9 @@ import Foundation
 
 struct FlightsManager {
     
-    // Update the appid with your API Key
     let flightURL = "http://api.aviationstack.com/v1/flights?access_key=c5970189867c88568b4dccb6ede6a42e&airline_iata=VA&flight_status=scheduled&limit=10"
     
-    func fetchFlight(cityName: String) {
+    func fetchFlight() {
         let urlString = "\(flightURL)"
         
         performRequest(urlString: urlString)
@@ -20,11 +19,11 @@ struct FlightsManager {
     
     
     func performRequest(urlString: String) {
-        // step1: create URL
+        
         if let url = URL(string: urlString) {
-            // step 2: create a URL session
+            
             let session = URLSession(configuration: .default)
-            // step 3: give URLSession a task
+           
             let task = session.dataTask(with: url) { (data, response, error) in
                 if error != nil {
                     print(error!)
@@ -37,7 +36,6 @@ struct FlightsManager {
                 }
             }
             
-            // step 4: start a task
             task.resume()
         }
     }
@@ -47,8 +45,7 @@ struct FlightsManager {
         let decoder = JSONDecoder()
         do {
             let decodedData = try decoder.decode(FlightsData.self, from: flightsData)
-            //print(decodedData.name)
-            //print(decodedData.main.temp)
+           
             print(decodedData.entry[0].flight.number)
             print(decodedData.entry[0].airline.name)
             print(decodedData.entry[0].departure.scheduled)
