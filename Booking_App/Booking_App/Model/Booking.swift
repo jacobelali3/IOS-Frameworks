c27101b618b28a6578e728dff6c5840bbc6a3cfa
Travis <main@192-168-1-115.tpgi.com.au>
2022-05-18 22:51:04 +1000

    added base functions for interacting with variables in model

--- Booking_App/Booking_App/Model/Booking.swift
+++ Booking_App/Booking_App/Model/Booking.swift
@@ -6,10 +6,38 @@
 //
 
 import Foundation
-struct Booking{
+class Booking{
     //keep this as a string, we will concat a string with some integer when instantiating.
     var bookingNumber : String = "NOT_SET"
     var flightName : String = "Some flight"
     var date : Date = .now
     var price : Double = 0.0
+    
+    func getbookingNumber() -> String{
+        return bookingNumber;
+    }
+    
+    func setBookingNumber(bookingNumber: String){
+        self.bookingNumber = bookingNumber;
+    }
+    
+    func getFlightName() -> String{
+        return flightName;
+    }
+    
+    func setFlightName(flightName: String){
+        self.flightName = flightName;
+    }
+    
+    func getDate() -> Date{
+        return date;
+    }
+    
+    func getPrice() -> Double{
+        return price;
+    }
+    
+    func setPrice(price: Double){
+        self.price = price;
+    }
 }
--- Booking_App/Booking_App/Model/Ticket.swift
+++ Booking_App/Booking_App/Model/Ticket.swift
@@ -6,9 +6,26 @@
 //
 
 import Foundation
-struct Ticket{
+class Ticket{
     //Combination of flight name + some random number
     var ticketNumber : String = "123"
     var flightNumber : String = "someFlightName"
     var seat : Int = -1
+    
+    func getTicketNumber() -> String{
+        return ticketNumber;
+    }
+    
+    func setTicketNumber(ticketNumber: String){
+        self.ticketNumber = ticketNumber;
+    }
+    
+    func getFlightNumber() -> String{
+        return flightNumber;
+    }
+    
+    func setFlightNumber(flightNumber: String){
+        self.flightNumber = flightNumber;
+    }
+    
 }
