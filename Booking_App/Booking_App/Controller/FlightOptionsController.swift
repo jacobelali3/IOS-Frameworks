e6ddc16f23de2c70060def88ee19bc7fb3c58448
Matthew <13524665@student.uts.edu.au>
2022-05-21 03:44:57 +1000

    Added functionality to flight options

--- Booking_App/Booking_App/Controller/FlightOptionsController.swift
+++ Booking_App/Booking_App/Controller/FlightOptionsController.swift
@@ -10,14 +10,120 @@ import UIKit
 
 class FlightOptionsController: UIViewController {
     
+    
+    @IBOutlet weak var fromButton: UIButton!
+    
+    @IBOutlet weak var toButton: UIButton!
+    
+    @IBOutlet weak var selectedDate: UIDatePicker!
+    
+    @IBAction func dateChanged(_ sender: Any) {
+        print(selectedDate.date)
+    }
+    
     var someUser : User = User()
     //etUserDate(_ day: Int,_ month: Int,_ year: Int,_ hour: Int,_ minute: Int){
     override func viewDidLoad() {
         super.viewDidLoad()
-        //let flightMana = FlightsManager()
-        //flightMana.fetchFlight()
+        var iataFrom: String?
+        var iataTo: String?
+        
+        let optionFrom = {(action : UIAction) in
+            print(action.title)
+            
+            switch action.title {
+            case "Canberra":
+                iataFrom = "CBR"
+                
+            case "Darwin":
+                iataFrom = "DRW"
+                
+            case "Brisbane":
+                iataFrom = "BNE"
+                
+            case "Adelaide":
+                iataFrom = "ADL"
+                
+            case "Hobart":
+                iataFrom = "HBA"
+                
+            case "Melbourne":
+                iataFrom = "MEL"
+                
+            case "Perth":
+                iataFrom = "PER"
+                
+            default:
+                iataFrom = "SYD"
+            }
+            
+            print(iataFrom ?? "")
+        }
+        
+        fromButton.menu = UIMenu(children: [
+            UIAction(title: "Sydney", handler: optionFrom),
+            UIAction(title: "Canberra", handler: optionFrom),
+            UIAction(title: "Darwin", handler: optionFrom),
+            UIAction(title: "Brisbane", handler: optionFrom),
+            UIAction(title: "Adelaide", handler: optionFrom),
+            UIAction(title: "Hobart", handler: optionFrom),
+            UIAction(title: "Melbourne", handler: optionFrom),
+            UIAction(title: "Perth", handler: optionFrom)
+        ])
+        
+        fromButton.showsMenuAsPrimaryAction = true
+        
+        let optionTo = {(action : UIAction) in
+            print(action.title)
+            
+            switch action.title {
+            case "Canberra":
+                iataTo = "CBR"
+                
+            case "Darwin":
+                iataTo = "DRW"
+                
+            case "Brisbane":
+                iataTo = "BNE"
+                
+            case "Adelaide":
+                iataTo = "ADL"
+                
+            case "Hobart":
+                iataTo = "HBA"
+                
+            case "Melbourne":
+                iataTo = "MEL"
+                
+            case "Perth":
+                iataTo = "PER"
+                
+            default:
+                iataTo = "SYD"
+            }
+            
+            print(iataTo ?? "")
+        }
+        
+        toButton.menu = UIMenu(children: [
+            UIAction(title: "Sydney", handler: optionTo),
+            UIAction(title: "Canberra", handler: optionTo),
+            UIAction(title: "Darwin", handler: optionTo),
+            UIAction(title: "Brisbane", handler: optionTo),
+            UIAction(title: "Adelaide", handler: optionTo),
+            UIAction(title: "Hobart", handler: optionTo),
+            UIAction(title: "Melbourne", handler: optionTo),
+            UIAction(title: "Perth", handler: optionTo)
+        ])
+        
+        
+        toButton.showsMenuAsPrimaryAction = true
     }
     
+   
+    
+    
 
+    
     
 }
